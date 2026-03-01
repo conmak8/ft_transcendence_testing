80b994fee85f1dcceda52b2fb2088a60483f3c8b
Robert Reimann <34074906+MM1132@users.noreply.github.com>
2026-03-01 21:35:18 +0100

    Our new commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+console.log(`Here is our VITE_ALLOWED_HOSTS: ${process.env.VITE_ALLOWED_HOSTS}`);
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   server: {
-    allowedHosts: [import.meta.env.VITE_ALLOWED_HOSTS],
+    allowedHosts: [process.env.VITE_ALLOWED_HOSTS],
     host: '0.0.0.0',
     port: 5173,
     watch: {
